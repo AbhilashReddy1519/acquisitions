ed72c4f9aaba83da9737b86c3eabf1c956b6e809
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-12-02 19:41:04 +0530

    adds loggers and middleware to app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 // This file is all about setting up the express application logic with right middlewares
 
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import logger from '#config/logger.js';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+// we are actually combining both out logging library through winston and morgan by passing over morgans messages into our logger  
+app.use(
+  morgan('combined', { stream: { write: msg => logger.info(msg.trim()) } })
+);
+
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions');
   res.status(200).send('Hello, World! from acquisitions application');
 });
 
