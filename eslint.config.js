ad21b7adabace62a2ddd6f6f5a26197988abc36b
AbhilashReddy1519 <abhilashreddy6300@gmail.com>
2025-12-02 18:48:14 +0530

    Set up Neon Postgres with Drizzle

--- eslint.config.js
+++ eslint.config.js
@@ -21,7 +21,7 @@ export default [
     },
     rules: {
       indent: ['error', 2, { SwitchCase: 1 }],
-      'linebreak-style': ['error', 'unix'],
+      'linebreak-style': ['error', process.platform === 'win32'? 'windows':'unix'],
       quotes: ['error', 'single'],
       semi: ['error', 'always'],
       'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
